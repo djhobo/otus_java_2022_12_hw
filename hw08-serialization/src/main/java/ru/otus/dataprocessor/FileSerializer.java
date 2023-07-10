b81565e3e63b6535f5ef080ac2a70c2e7c9f47d3
djhobo <djhobo@gmail.com>
2023-07-10 13:13:53 +0500

    hw08-serialization

--- hw08-serialization/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ hw08-serialization/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,14 +1,26 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+
+import java.io.*;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
 
+    private final String fileName;
+
     public FileSerializer(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
     public void serialize(Map<String, Double> data) {
         //формирует результирующий json и сохраняет его в файл
+        try (Writer pw = new FileWriter(fileName)) {
+            String gson = new Gson().toJson(data);
+            pw.write(gson);
+        } catch (IOException e) {
+            throw new FileProcessException(e);
+        }
     }
 }
--- hw08-serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw08-serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,24 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+import jakarta.json.Json;
 import ru.otus.model.Measurement;
+
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
+    private final String fileName;
 
     public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
     }
 
-    @Override
+    @Override   //читает файл, парсит и возвращает результат
     public List<Measurement> load() {
-        //читает файл, парсит и возвращает результат
-        return null;
+        try (var jsonReader = Json.createReader(this.getClass().getClassLoader().getResourceAsStream(fileName))) {
+            return List.of(new Gson().fromJson(jsonReader.readArray().toString(), Measurement[].class));
+        } catch (RuntimeException re) {
+            throw new FileProcessException(re);
+        }
     }
 }
